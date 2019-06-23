13ef944cf4da5e671ede2c3cedaed1198d1cc231
Charlie Paisan <cppaisan@gmail.com>
2019-06-23 10:39:05 -0700

    [chore] Add POST endpoint to handle document upload and add multer to save files

--- mock_api/server.js
+++ mock_api/server.js
@@ -1,10 +1,15 @@
 const jsonServer = require("json-server");
+const multer = require("multer");
+const uuid = require("uuid");
 let documents = require("./documents.json");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const upload = multer({ dest: "mock_api/uploads" });
+
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 // Get routes
 server.get("/documents/:name?", (req, res) => {
@@ -32,6 +37,15 @@ server.delete("/documents/:id", (req, res) => {
   // on the server
   documents = documents.filter(({ id: docId }) => docId !== id);
   res.sendStatus(200);
+});
+
+// Post routes
+server.post("/document", upload.any(), (req, res) => {
+  const { files: [file = {}] = {} } = req;
+  const { originalname, size } = file || {};
+  const formattedFile = { id: uuid(), name: originalname, size };
+  documents = [formattedFile, ...documents];
+  res.json(formattedFile);
 });
 
 server.use(router);
